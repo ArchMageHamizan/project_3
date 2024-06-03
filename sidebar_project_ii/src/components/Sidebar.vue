0ca2bb420ca75afff11a70cc6d49c7ae0712625b
hamizan <hamizan@hla-groupp.com>
2024-06-03 16:58:08 +0800

    initial commit

---
+++ sidebar_project_ii/src/components/Sidebar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="relative flex  min-h-screen">
+        <!-- ====================== sidebar ================== -->
+        <div class="bg-green-600 text-cyan-100">Sidebar</div>
+        <!-- ====================== main content ============= -->
+        <div>
+            <div>header</div>
+            <div>content</div>
+            <div>footer</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style>
+
+</style>
